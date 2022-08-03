8efb58f9ac7e721814522efd2c9d95e5a9f05b78
sanZAngjay <110442922+sanZAngjay@users.noreply.github.com>
2022-08-03 15:04:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['62895635507411','62895336722950','6287721317870','6288292024190']
+global.premium = ['62895635507411']
+global.packname = 'SanZ'
+global.author = '@ SanZ'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
